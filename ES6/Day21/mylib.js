7cfe5f3cce956c8e4b7a6550a063e5f50f76c8e0
Aman Tiwari <amantiwari8861@gmail.com>
2024-04-16 20:46:00 +0530

    es6 concepts

---
+++ ES6/Day21/mylib.js
@@ -0,0 +1,26 @@
+let data=10;
+let a=55;
+// console.log("hello");
+// function add(a,b) {
+// export default function add(a,b) {
+//         return a+b;
+// }
+class Animal
+{
+    constructor(name)
+    {
+        this.name=name;
+    }
+    printName()
+    {
+        console.log("the animal name is:"+this.name);
+    }
+}
+// export default Animal;
+// export default data
+// export default add;
+// export default data,a
+
+export {a,data,Animal}
+
+
