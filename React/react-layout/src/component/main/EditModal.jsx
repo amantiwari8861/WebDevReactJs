91c87080080091db9bf6a6600de483cfb393a9b4
Aman Tiwari <amantiwari8861@gmail.com>
2024-05-11 22:25:40 +0530

    fixed edit modal

--- React/react-layout/src/component/main/EditModal.jsx
+++ React/react-layout/src/component/main/EditModal.jsx
@@ -1,34 +1,57 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Modal } from "bootstrap";
 
-const EditModal = ({method,product}) => {
-    let modelref=useRef(null);
-    useEffect(()=>{
-        console.log(modelref);
-        console.log(modelref.current);
-        // modelref.current.modal("show")
-    },[])
+const EditModal = ({ method, product }) => {
+    let modalRef = useRef(null);
+    const [editedProduct, setEditedProduct] = useState({ ...product });
+
+    useEffect(() => {
+        const modal = new Modal(modalRef.current);
+        modal.show();
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditedProduct({
+            ...editedProduct,
+            [name]: value,
+        });
+    };
+
+    const handleSave = () => {
+        method(editedProduct);
+    };
 
     return (
-        <div className="modal fade" ref={modelref} id="editModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            {/* {$("#editModal").modal("show")} */}
+        <div className="modal fade" ref={modalRef} tabIndex="-1">
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Edit Product</h5>
+                        <h5 className="modal-title">Edit Product</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        {console.log(product)}
-                        {console.log(method)}
-                        {/* {edit(product)} */}
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="image">Image Link:</label>
+                            <input type="text" className="form-control" id="image" name="image" value={editedProduct.image} onChange={handleInputChange} />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="name">Product Name</label>
+                            <input type="text" className="form-control" id="name" name="name" value={editedProduct.name} onChange={handleInputChange} />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="price">Price</label>
+                            <input type="number" className="form-control" id="price" name="price" value={editedProduct.price} onChange={handleInputChange} />
+                        </div>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">edit</button>
+                        <button type="button" className="btn btn-primary" onClick={handleSave}>Save</button>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+
 export default EditModal;
