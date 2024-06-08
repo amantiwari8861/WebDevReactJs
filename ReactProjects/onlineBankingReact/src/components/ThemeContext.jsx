83b888abd260d7ced361e4331ca2c8d892b8f43f
Aman Tiwari <amantiwari8861@gmail.com>
2024-06-08 20:31:13 +0530

    light and dark mode implemented

---
+++ ReactProjects/onlineBankingReact/src/components/ThemeContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useContext, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  // Check system preference on initial load
+  const getInitialTheme = () => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    }
+    return 'light';
+  };
+
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  const toggleTheme = () => {
+    // console.log(theme);
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
--- ReactProjects/onlineBankingReact/src/main.jsx
+++ ReactProjects/onlineBankingReact/src/main.jsx
@@ -6,11 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'font-awesome/css/font-awesome.min.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './components/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 )
